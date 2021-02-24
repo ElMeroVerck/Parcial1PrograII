7cbfab1b2ae086ba6f8bb332d35abe11b06c2ac4
ElMeroVerck <erickemanuelolmedo@gmail.com>
2021-02-23 21:11:10 -0600

    parte2

--- pregunta3/main.js
+++ pregunta3/main.js
@@ -1,18 +1,29 @@
 
 function myCreateFunction() {
-    var nPartido = document.getElementById("Pname").Value;
-    var Orientacion = document.getElementById("Orientacion").Value;
-    var votos = document.getElementById("Vnumber").Value
+    var nPartido = document.getElementById("Pname").value;
+    var Orientacion = document.getElementById("Orientacion").value;
+    var votos = document.getElementById("Vnumber").value;
 
     var table = document.getElementById("partidos");
 
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
+    
     var cell3 = row.insertCell(2);
-    cell1.innerHTML = "hola";
-    cell2.innerHTML = Orientacion.Value;
-    cell3.innerHTML = votos;
+    
+
+    cell1.innerHTML = nPartido;
+    cell2.innerHTML = Orientacion;
+    if (votos > 50) {
+        cell3.innerHTML = "50+"
+     } else {
+        cell3.innerHTML = votos;
+     }
+
+
+    
+    
 }
 
 
